16a2e16acdfe51f095c205f2452d3bcd6e064936
andreea-qa <andreea.draniceanu@gmail.com>
2022-11-16 22:49:18 +0200

    Add project files.

---
+++ RefreshSelenium/PlaygroundHomePage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshSelenium
+{
+    internal class PlaygroundHomePage
+    {
+        private IWebDriver driver;
+        public PlaygroundHomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement SimpleFormLink => driver.FindElement(By.LinkText("Simple Form Demo"));
+        public IWebElement RadioButtonsLink => driver.FindElement(By.LinkText("Radio Buttons Demo"));
+
+      
+        internal void GoToSimpleFormDemoPage()
+        {
+            SimpleFormLink.Click();
+        }
+        internal void GoToRadioButtonsDemoPage()
+        {
+            RadioButtonsLink.Click();
+        }
+
+        internal void NavigateToLink(IWebElement pageLink)
+        {
+            pageLink.Click();
+        }
+
+    }
+}
---
+++ RefreshSelenium/RefreshTest.cs
@@ -0,0 +1,52 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Safari;
+
+namespace RefreshSelenium
+{
+    public class RefreshTest
+    {
+
+        private static IWebDriver driver;
+        private static readonly string gridURL = "@hub.lambdatest.com/wd/hub";
+        private static readonly string LT_USERNAME = Environment.GetEnvironmentVariable("LT_USERNAME");
+        private static readonly string LT_ACCESS_KEY = Environment.GetEnvironmentVariable("LT_ACCESS_KEY");
+
+        [SetUp]
+        public void Setup()
+        {
+            SafariOptions capabilities = new SafariOptions();
+            capabilities.BrowserVersion = "15.0";
+            Dictionary<string, object> ltOptions = new Dictionary<string, object>();
+            ltOptions.Add("username", LT_USERNAME);
+            ltOptions.Add("accessKey", LT_ACCESS_KEY);
+            ltOptions.Add("visual", true);
+            ltOptions.Add("platformName", "MacOS Monterey");
+            ltOptions.Add("build", "Refresh");
+            ltOptions.Add("project", "RefrestTest");
+            ltOptions.Add("w3c", true);
+            ltOptions.Add("plugin", "c#-nunit");
+            capabilities.AddAdditionalOption("LT:Options", ltOptions);
+            driver = new RemoteWebDriver(new Uri($"https://{LT_USERNAME}:{LT_ACCESS_KEY}{gridURL}"), capabilities);
+        }
+
+        [Test]
+        public void RefreshPage()
+        {
+            PlaygroundHomePage homePage = new PlaygroundHomePage(driver);
+            driver.Navigate().GoToUrl("https://www.lambdatest.com/selenium-playground/");
+            homePage.NavigateToLink(homePage.SimpleFormLink);
+            driver.Navigate().Back();
+            driver.Navigate().Refresh();
+            //homePage.RadioButtonsLink.SendKeys(Keys.F5);
+            //driver.Navigate().GoToUrl(driver.Url);
+            homePage.NavigateToLink(homePage.RadioButtonsLink);  
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
